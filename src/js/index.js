e6c21f36877f84fb8ca14b62159ea14185b84fd6
baran beyazıt <byztmain2121@gmail.com>
2022-01-14 02:36:09 +0300

    i am finished works page

--- src/js/index.js
+++ src/js/index.js
@@ -24,7 +24,39 @@ if(go.display == "none"){
 
 
 }  
-
+var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    loopFillGroupWithBlank: true,
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+    breakpoints: {
+  400: {
+    slidesPerView: 1,
+    slidesPerGroup: 1,
+    spaceBetween: 30,
+  },
+  768: {
+    slidesPerView: 2,
+    slidesPerGroup: 1,
+    spaceBetween: 30,
+  },
+  1200: {
+   slidesPerView: 3,
+   slidesPerGroup: 3,
+   spaceBetween: 30,
+  },
+  },
+    keyboard: {
+        enabled: true,
+        onlyInViewport: false,
+     },
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+  });
 
 /*
 
